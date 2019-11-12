15d12a604578d48a9c359fb359dd65a7f94e0227
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-11-12 15:22:22 +0000

    First Commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,37 @@
+describe( 'Fizzbuzz', function() {
+    var fizzBuzz; 
+
+    beforeEach(function() {
+        fizzBuzz = new FizzBuzz();
+    });
+
+    describe('multiples of 3', function() {
+        it( 'fizzes for 3', function() {
+            expect(fizzBuzz.play(3)).toEqual('Fizz');
+        });
+
+    //     it('fizzes for 6', function() {
+    //         expect(fizzBuzz.play(6)).toEqual('Fizz');
+    //     });
+
+    // });
+
+    // describe('multiples of 5', function(){
+    //     it('buzzes for 5', function(){
+    //         expect(fizzBuzz.play(5)).toEqual('Buzz');
+    //     });
+        
+    //     it ('buzzes for 10', function(){
+    //         expect(fizzBuzz.play(10)).toEqual('Buzz'); 
+    //     });
+    // });
+    
+    // describe('multiples of 3 and 5', function(){
+    //     it('fizzbuzzes for 15', function(){
+    //         expect(fizzBuzz.play(30)).toEqual('FizzBuzz');
+    //     })
+    // })
+
+
+});
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,31 @@
+function FizzBuzz() { 
+}
+
+Fizzbuzz.prototype.play = function(num) { 
+//   if num % 3 === 0 && num % 5
+//     return "fizzbuzz"
+// });
+ 
+
+
+
+        if(this._isDivisibleBy(15, number)) {
+        return 'FizzBuzz' };
+    } else if (this._isDivisibleBy(5, number)) {
+        return 'Buzz';
+    } else if (this._isDivisibleBy(3, number)) {
+        return 'Fizz';
+    } else {
+        return number;
+    } 
+}
+
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+    return number % divisor === 0
+}
+
+var fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+}

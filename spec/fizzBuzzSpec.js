79209cfc740dd6148865a30c4cd284ff8514807f
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-11-12 20:09:11 +0000

    FizzBuzz almost Complete

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,37 +1,66 @@
 describe( 'Fizzbuzz', function() {
-    var fizzBuzz; 
+    // var fizzBuzz; 
 
     beforeEach(function() {
         fizzBuzz = new FizzBuzz();
     });
-
-    describe('multiples of 3', function() {
-        it( 'fizzes for 3', function() {
-            expect(fizzBuzz.play(3)).toEqual('Fizz');
-        });
-
-    //     it('fizzes for 6', function() {
-    //         expect(fizzBuzz.play(6)).toEqual('Fizz');
-    //     });
-
-    // });
-
-    // describe('multiples of 5', function(){
-    //     it('buzzes for 5', function(){
-    //         expect(fizzBuzz.play(5)).toEqual('Buzz');
-    //     });
-        
-    //     it ('buzzes for 10', function(){
-    //         expect(fizzBuzz.play(10)).toEqual('Buzz'); 
-    //     });
-    // });
     
-    // describe('multiples of 3 and 5', function(){
-    //     it('fizzbuzzes for 15', function(){
-    //         expect(fizzBuzz.play(30)).toEqual('FizzBuzz');
-    //     })
-    // })
+    it('take 3 and return fizz', function() {
+        expect(fizzBuzz.play(3)).toEqual('fizz');
+    });
 
+    it ('take 5 & return buzz', function(){
+        expect(fizzBuzz.play(5)).toEqual('buzz');
+    });
+
+    it ('divisible by both 3 & 5', function(){
+        expect(fizzBuzz.play(15)).toEqual('fizzBuzz');
+    });
+
+    it ('return num if any other num given', function(){
+        expect(fizzBuzz.play()).toEqual('num')
+    });
 
 });
-});
+
+    // it('take in 3 and return 3', function() {
+    //     expect(fizzBuzz.play(3)).toEqual(3);
+    // });
+
+
+
+    // it('take in 1 and return 1', function() { 
+    //     expect(fizzBuzz.play().toEqual(1))
+
+    // });
+
+
+//     describe('multiples of 3', function() {
+//         it( 'fizzes for 3', function() {
+//             expect(fizzBuzz.play(3)).toEqual('Fizz');
+//         });
+
+//         it('fizzes for 6', function() {
+//             expect(fizzBuzz.play(6)).toEqual('Fizz');
+//         });
+
+//     });
+
+//     describe('multiples of 5', function(){
+//         it('buzzes for 5', function(){
+//             expect(fizzBuzz.play(5)).toEqual('Buzz');
+//         });
+        
+//         it ('buzzes for 10', function(){
+//             expect(fizzBuzz.play(10)).toEqual('Buzz'); 
+//         });
+//     });
+    
+//     describe('multiples of 3 and 5', function(){
+//         it('fizzbuzzes for 15', function(){
+//             expect(fizzBuzz.play(30)).toEqual('FizzBuzz');
+//         });
+//     });
+
+
+// });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,31 +1,52 @@
 function FizzBuzz() { 
-}
+};
 
-Fizzbuzz.prototype.play = function(num) { 
-//   if num % 3 === 0 && num % 5
+FizzBuzz.prototype.play = function(num) { 
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'fizzBuzz'
+    };
+    
+    if (num % 3 === 0) { 
+        return 'fizz'
+    }; 
+    
+    if (num % 5 === 0) {
+        return 'buzz'
+    };
+    if (num === num) {
+        return num
+    }
+   
+};
+
+
+// if num % 3 === 0 && num % 5
 //     return "fizzbuzz"
-// });
+//  });
  
 
 
 
-        if(this._isDivisibleBy(15, number)) {
-        return 'FizzBuzz' };
-    } else if (this._isDivisibleBy(5, number)) {
-        return 'Buzz';
-    } else if (this._isDivisibleBy(3, number)) {
-        return 'Fizz';
-    } else {
-        return number;
-    } 
-}
 
-FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
-    return number % divisor === 0
-}
 
-var fizzBuzz = new FizzBuzz();
 
-for (var i = 1; i <= 100; i++) {
-  console.log(fizzBuzz.play(i));
-}
+//         if(this._isDivisibleBy(15, number)) {
+//         return 'FizzBuzz' };
+//     } else if (this._isDivisibleBy(5, number)) {
+//         return 'Buzz';
+//     } else if (this._isDivisibleBy(3, number)) {
+//         return 'Fizz';
+//     } else {
+//         return number;
+//     } 
+// }
+
+// FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+//     return number % divisor === 0
+// }
+
+// var fizzBuzz = new FizzBuzz();
+
+// for (var i = 1; i <= 100; i++) {
+//   console.log(fizzBuzz.play(i));
+// }
